134e6f1c78e7eb9b1a553234c14a8ea7099d6823
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-07-12 16:55:02 -0300

    Update script.js

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var agora = new Date()
+    var hora = agora.getHours()
+    var min = agora.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${min} Minutos.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+        img.src = 'fotomanha.png'  
+        document.body.style.background = '#c7b087' 
+        
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+        img.src = 'fototarde.png' 
+        document.body.style.background = '#cb9056'
+        
+    } else {
+        //BOA NOITE!
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2b6b97'
+    }
+}
