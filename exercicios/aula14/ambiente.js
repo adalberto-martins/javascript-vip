6e2d13947445c950f0d38567fa9dcd3531b513c5
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-07-14 20:19:01 -0300

    Create ambiente.js

---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,21 @@
+console.log(`Vai começar...`)
+for(var c =1; c <=5; c++) {
+    console.log(`c`)
+}
+console.log(`FIM!`)
+
+/* teste logico no final
+var c = 1
+do {
+    console.log(`c`)
+    c++
+} while (c <= 5)
+*/
+
+/* teste logico no inicio
+var c = 1
+while (c <= 5) {
+    console.log(`c`)
+    c++
+}
+*/
