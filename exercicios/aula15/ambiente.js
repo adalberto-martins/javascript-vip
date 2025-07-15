86dae4e4ab5b12c6aecfe371f9fbb71437e87e04
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-07-14 23:24:59 -0300

    Create ambiente.js

---
+++ exercicios/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let nun = [4, 5, 6, 7, 8]
+nun.sort()
+console.log(nun)
+console.log(`O vetor tem ${nun.length} posições`)
+console.log(`O primeiro valor do vetor é ${nun[0]}`)
+
+for(let pos = 0; pos < num.length;pos++) {
+    console.log(nun[pos])
+}
