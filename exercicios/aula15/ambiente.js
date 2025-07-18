4830086121235d192f2fcfa7e3ff7191b25d3f41
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-07-18 13:19:38 -0300

    Update ambiente.js

--- exercicios/aula15/ambiente.js
+++ exercicios/aula15/ambiente.js
@@ -1,9 +1,12 @@
-let nun = [4, 5, 6, 7, 8]
+let nun = [5, 8, 2, 9, 3]
+nun.push(1)
 nun.sort()
 console.log(nun)
 console.log(`O vetor tem ${nun.length} posições`)
 console.log(`O primeiro valor do vetor é ${nun[0]}`)
-
-for(let pos = 0; pos < num.length;pos++) {
-    console.log(nun[pos])
+let pos = nun.indexOf(8)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 esta na posição ${pos}`)
 }
