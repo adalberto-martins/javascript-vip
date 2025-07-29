293fd46fec7f5e7e9c9cd8a66a7dd6bf6315aa71
adalberto-martins <145614606+adalberto-martins@users.noreply.github.com>
2025-07-29 15:11:02 -0300

    Update script.js
    incluido saudação

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -4,20 +4,26 @@ function carregar() {
     var agora = new Date()
     var hora = agora.getHours()
     var min = agora.getMinutes()
-    msg.innerHTML = `Agora são ${hora} horas e ${min} Minutos.`
+    var saudacao = ''
+   
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
+        saudacao = 'BOM DIA!'
         img.src = 'fotomanha.png'  
         document.body.style.background = '#c7b087' 
         
     } else if (hora >= 12 && hora < 18) {
         //BOA TARDE!
+        saudacao = 'BOA TARDE!'
         img.src = 'fototarde.png' 
         document.body.style.background = '#cb9056'
         
     } else {
         //BOA NOITE!
+        saudacao = 'BOA NOITE!'
         img.src = 'fotonoite.png'
         document.body.style.background = '#2b6b97'
     }
+
+    msg.innerHTML = `Agora são ${hora} horas e ${min} Minutos. ${saudacao}`
 }
